5b638486eff51ae14a5ec8eabe775ff01e006d59
Charles <coreanq@gmail.com>
2021-12-31 11:53:48 +0900

    get account info test complete

--- digital_garbage_collector.py
+++ digital_garbage_collector.py
@@ -1,60 +1,85 @@
-# Define the network client
-from xrpl.clients import JsonRpcClient
-JSON_RPC_URL = "https://s.altnet.rippletest.net:51234/"
-client = JsonRpcClient(JSON_RPC_URL)
+import json 
 
-# Create a wallet using the testnet faucet:
-# https://xrpl.org/xrp-testnet-faucet.html
-from xrpl.wallet import generate_faucet_wallet
-test_wallet = generate_faucet_wallet(client, debug=True)
+# # Define the network client
+# from xrpl.clients import JsonRpcClient
+# from xrpl.core.addresscodec.codec import SEED_LENGTH
+# JSON_RPC_URL = "https://s.altnet.rippletest.net:51234/"
+# client = JsonRpcClient(JSON_RPC_URL)
 
-print('public key {}'.format(test_wallet.public_key) )
-print('private key {}'.format(test_wallet.private_key) )
-print('seed {}'.format(test_wallet.seed) )
-print('sequence {}'.format(test_wallet.sequence) )
+# # Create a wallet using the testnet faucet:
+# # https://xrpl.org/xrp-testnet-faucet.html
+# from xrpl.wallet import generate_faucet_wallet
+# test_wallet = generate_faucet_wallet(client, debug=True)
 
-# Create an account str from the wallet
-test_account = test_wallet.classic_address
+# print(f'{test_wallet.public_key=}')
+# print(f'{test_wallet.private_key=}')
+# print(f'{test_wallet.seed=}')
+# print(f'{test_wallet.sequence=}') 
 
-# Derive an x-address from the classic address:
-# https://xrpaddress.info/
-from xrpl.core import addresscodec
-test_xaddress = addresscodec.classic_address_to_xaddress(test_account, tag=12345, is_test_network=True)
-print("\nClassic address:\n\n", test_account)
-print("X-address:\n\n", test_xaddress)
+# # Create an account str from the wallet
+# test_account = test_wallet.classic_address
 
-# Look up info about your account
-from xrpl.models.requests.account_info import AccountInfo
-acct_info = AccountInfo(
-    account=test_account,
-    ledger_index="validated",
-    strict=True,
-)
-response = client.request(acct_info)
-result = response.result
-print("response.status: ", response.status)
-import json
-print(json.dumps(response.result, indent=4, sort_keys=True))
+# # Derive an x-address from the classic address:
+# # https://xrpaddress.info/
+# from xrpl.core import addresscodec
+# test_xaddress = addresscodec.classic_address_to_xaddress(test_account, tag=12345, is_test_network=True)
+# print("\nClassic address:\n\n", test_account)
+# print("X-address:\n\n", test_xaddress)
 
 
-# Prepare payment
-from xrpl.models.transactions import Payment
-from xrpl.utils import xrp_to_drops
-my_tx_payment = Payment(
-    account=test_account,
-    amount=xrp_to_drops(22),
-    destination="rPT1Sjq2YGrBMTttX4GZHjKu9dyfzbpAYe",
-)
-
-# Sign the transaction
-from xrpl.transaction import safe_sign_and_autofill_transaction
-
-my_tx_payment_signed = safe_sign_and_autofill_transaction(my_tx_payment, test_wallet, client)
-
-# Submit and send the transaction
-from xrpl.transaction import send_reliable_submission
-
-tx_response = send_reliable_submission(my_tx_payment_signed, client)
+# # Look up info about your account
+# from xrpl.models.requests.account_info import AccountInfo
+# acct_info = AccountInfo(
+#     account=test_account,
+#     ledger_index="validated",
+#     strict=True,
+# )
+# response = client.request(acct_info)
+# result = response.result
+# print("response.status: ", response.status)
+# import json
+# print(json.dumps(response.result, indent=4, sort_keys=True))
 
 
-print(tx_response)
+# # Prepare payment
+# from xrpl.models.transactions import Payment
+# from xrpl.utils import xrp_to_drops
+# my_tx_payment = Payment(
+#     account=test_account,
+#     amount=xrp_to_drops(22),
+#     destination="rPT1Sjq2YGrBMTttX4GZHjKu9dyfzbpAYe",
+# )
+
+# # Sign the transaction
+# from xrpl.transaction import safe_sign_and_autofill_transaction
+
+# my_tx_payment_signed = safe_sign_and_autofill_transaction(my_tx_payment, test_wallet, client)
+
+# # Submit and send the transaction
+# from xrpl.transaction import send_reliable_submission
+
+# tx_response = send_reliable_submission(my_tx_payment_signed, client)
+
+
+# print(tx_response)
+
+
+if __name__ == "__main__":
+	from xrpl import account
+	from xrpl.clients import JsonRpcClient
+	JSON_RPC_URL = "https://s2.ripple.com:51234/"
+	client = JsonRpcClient(JSON_RPC_URL)
+	wallet_address1 = 'rPnYrAAwgR7YQ8qqL5AexMBCZ2A7826rMN'
+	wallet_address2 = 'r49zsuZWw2TLuBm9e5xNePwBnWSJzEXSiP'
+
+	response = account.get_account_info(wallet_address1, client )
+	print(json.dumps(response.result, indent=4, sort_keys=True))
+
+	response = account.get_account_info(wallet_address2, client )
+	print(json.dumps(response.result, indent=4, sort_keys=True))
+
+
+	from xrpl.wallet import Wallet
+	test_wallet = Wallet(seed="sn3nxiW7v8KXzPzAqzyHXbSSKNuN9", sequence=16237283)
+	print(test_wallet.classic_address) # "rMCcNuTcajgw7YTgBy1sys3b89QqjUrMpH"
+	pass
